ad56ef659fb23d7d4bb2d54c175b69d0a547a0af
Andrew Yupin <brolo1341@gmail.com>
2024-05-16 12:45:44 +0300

    changed favicon, added env ( prod/dev )

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
     production: true,
-    // apiUrl: 'https://node-implementation.vercel.app/api',
+    apiUrl: 'https://node-implementation.vercel.app/api',
   };
 
   
